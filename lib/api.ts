f0e52d9bd49c4856c7b39479416d157a88dd3e53
mateusrfc2025 <mateusramalhofc@gmail.com>
2026-09-03 11:36:26 -0300

    Troca para a URL raw do vagas .jason em lib/api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import { Vaga } from "./tipos";
 
 export default async function listarVagas() {
-  const resposta = await fetch("https://.../dados/vagas.json");
+  const resposta = await fetch("https://raw.githubusercontent.com/mateusrfc2025/lequedevagas-web/refs/heads/main/dados/vagas.json");
   const vagas: Vaga[] = await resposta.json();
 
   return vagas
